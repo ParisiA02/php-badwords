11be82238ade087f2ba3485e19276ddf66661c3f
Andrea Parisi <andreaparisi4502@gmail.com>
2021-12-17 14:26:50 +0100

    commit ex.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $nome ="Andrea";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Il mio nome e' <?php echo $nome ?> ed e' lungo <?php echo strlen($nome) ?> caratteri
+    </h1>
+</body>
+</html>
